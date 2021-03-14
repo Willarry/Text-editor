8a0881bb64e3ceeb41f97fb74a7888759b217bc6
William Larry <77531807+Willarry@users.noreply.github.com>
2021-03-14 07:48:28 -0700

    Add files via upload

---
+++ Text-Editor/script.js
@@ -0,0 +1,13 @@
+let output = document.getElementById('output');
+let buttons = document.getElementsByClassName('tool--btn');
+for (let btn of buttons) {
+	btn.addEventListener('click', () => {
+		let cmd = btn.dataset['command'];
+		if(cmd === 'createlink') {
+			let url = prompt("Enter the link here: ", "http:\/\/");
+			document.execCommand(cmd, false, url);
+		} else {
+			document.execCommand(cmd, false, null);
+		}
+	})
+}
